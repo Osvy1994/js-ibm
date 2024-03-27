ea4d8bacdd35013014f0ab1df0ed5e3672c705dc
Osvy1994 <Osvy1994@users.noreply.github.com>
2024-03-27 11:19:54 -0400

    created restaurantMenu

---
+++ restaurantMenu/restaurantMenu.js
@@ -0,0 +1,39 @@
+const breakfastMenu = [
+  'Pancakes- $12',
+  'Eggs Benedict -$22.99',
+  'Oatmeal -$21.99',
+  'Frittata -$15',
+]
+const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon']
+const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad']
+
+const breakfastMenuItemsHTML = breakfastMenu
+  .map((item, index) => {
+    return `<p>Item ${index + 1}: ${item}</p>`
+  })
+  .join('')
+
+document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML
+
+let mainCourseItem = ''
+
+mainCourseMenu.forEach((item, index) => {
+  mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`
+})
+
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem
+
+let dessertMenuItem = ''
+
+for (let i = 0; i < dessertMenu.length; i++) {
+  dessertMenuItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`
+}
+
+document.getElementById('dessertMenuItems').innerHTML = dessertMenuItem
+
+document.getElementById('breakfastTotalItems').innerText =
+  `Total Items: ${breakfastMenu.length}`
+document.getElementById('maincourseTotalItems').innerText =
+  `Total Items: ${mainCourseMenu.length}`
+document.getElementById('dessertTotalItems').innerText =
+  `Total Items: ${dessertMenu.length}`
